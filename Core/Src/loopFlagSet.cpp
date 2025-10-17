4a551900eba5deacea40781cb394cd101860a176
yuukou-338 <s253614v@st.go.tuat.ac.jp>
2025-10-17 16:54:48 +0900

    Create loopFlagSet.cpp

---
+++ Core/Src/loopFlagSet.cpp
@@ -0,0 +1,14 @@
+/*
+ * loopFlagSet.cpp
+ *
+ *  Created on: Oct 13, 2025
+ *      Author: yuukou
+ */
+
+#include "loopFlagset.hpp"
+#include "flight_data.hpp"
+
+void loopFlagSet(flightdata::FlightData* data){
+
+	data->switch_flag = true;
+}
