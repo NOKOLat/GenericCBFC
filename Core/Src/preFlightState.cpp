12a76397d31664542dd9b2b9505b64f3064d0691
yuukou-338 <s253614v@st.go.tuat.ac.jp>
2025-10-17 17:06:00 +0900

    Update preFlightState.cpp

--- Core/Src/preFlightState.cpp
+++ Core/Src/preFlightState.cpp
@@ -15,9 +15,9 @@ void preFlightState(flightdata::FlightData* data){
 		printf("pre flight state \r\n");
 		HAL_Delay(1000);
 
-		while(data->current_state != flightdata::state::FLIGHT_STATE){
+		while(data->current_state != flightdata::State::FLIGHT_STATE){
 
-			data->current_state = flightdata::state::FLIGHT_STATE;
+			data->current_state = flightdata::State::FLIGHT_STATE;
 			HAL_Delay(50);
 		}
 
