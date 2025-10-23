103012fba663a0795927b11fcc5f3a13618d4df8
yuukou-338 <s253614v@st.go.tuat.ac.jp>
2025-10-23 12:46:18 +0900

    Update flight_data.hpp
    arm, flight スイッチのフラグを追加
    bool arm_on = false;
    bool flight_on = false;

--- Core/Inc/flight_data.hpp
+++ Core/Inc/flight_data.hpp
@@ -33,6 +33,9 @@ struct flightdata {
 		  uint16_t sbus_channel_data[10];
 		  uint8_t receive_buffer[25] = {};
 
+		  bool arm_on = false;
+		  bool flight_on = false;
+
 		  bool switch_flag = false;
 
 		  float accel[3];
