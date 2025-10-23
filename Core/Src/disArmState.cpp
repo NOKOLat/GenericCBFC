9a851723b7105b1c0cefc5c6473ef18fc5215a4e
yuukou-338 <s253614v@st.go.tuat.ac.jp>
2025-10-23 18:35:19 +0900

    Update disArmState.cpp
    フラグによる状態遷移管理を追加

--- Core/Src/disArmState.cpp
+++ Core/Src/disArmState.cpp
@@ -7,18 +7,35 @@
 
 #include "main.h"
 #include "stdio.h"
+
+//各種データ
 #include "flight_data.hpp"
+#include "sbusDecode.hpp"
 
 //disArm 時に呼ばれる
 void disArmState(flightdata::FlightData* data){
 
-		printf("disarm state \r\n");
-		HAL_Delay(1000);
+	//現在の状態名を表示
+	printf("disarm state \r\n");
+	HAL_Delay(1000);
 
+	/*
+	 * ここに
+	 * disarm時の
+	 * 処理を
+	 * 記述する
+	 */
+
+	//armスイッチがオンになったら、PRE_ARM_STATE へ移行
+	if(data->arm_on == true){
+
+	//current_state が PRE_ARM_STATE 一致するまで書き込み
 		while(data->current_state != flightdata::State::PRE_ARM_STATE){
 
 				data->current_state = flightdata::State::PRE_ARM_STATE;
 				HAL_Delay(50);
 		}
+	}
+
 
 }
