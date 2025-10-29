242975321c0e378daa23182301bbe8dac4647389
yuukou-338 <s253614v@st.go.tuat.ac.jp>
2025-10-29 14:00:21 +0900

    Create failsafeState.cpp

---
+++ Core/Src/failsafeState.cpp
@@ -0,0 +1,12 @@
+#include "main.h"
+#include "stdio.h"
+
+
+//各種データ
+#include "flight_data.hpp"
+#include "sbusDecode.hpp"
+
+void failsafeState(flightdata::FlightData* data){
+
+	printf("fail safe state \r\n");
+}
